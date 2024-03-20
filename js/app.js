4a52f56f095cb5370931e18307e25a7478f800df
Jasczlv <ianis.cozlov@gmail.com>
2024-03-20 16:44:47 +0100

    ADD js

--- js/app.js
+++ js/app.js
@@ -168,3 +168,58 @@ console.log(idSize);
 //     idSize.push(idMaggiore);
 //   }
 // }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*SNACK 3
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal*/
+
+const arrayBici = [
+  {
+    bici: "mountain bike",
+    pesoInKg: "14",
+  },
+  {
+    bici: "graziella",
+    pesoInKg: "16",
+  },
+  {
+    bici: "bmx",
+    pesoInKg: "9",
+  },
+  {
+    bici: "bici da corsa",
+    pesoInKg: "7",
+  },
+  {},
+];
+
+let arrayPeso = ([
+  { pesoInKg: peso1 },
+  { pesoInKg: peso2 },
+  { pesoInKg: peso3 },
+  { pesoInKg: peso4 },
+] = arrayBici);
+
+for (let i = 0; i < arrayPeso.length; i++) {
+  let pesoCorrente = parseInt(arrayPeso[i].pesoInKg);
+  let pesoSuccessivo = parseInt(arrayPeso[i++].pesoInKg);
+  if (pesoCorrente < pesoSuccessivo) {
+    console.log("asd");
+  } else {
+    console.log("qwe");
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*SNACK 4
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+BONUS
+Stampare in pagina oltre che in console!*/
